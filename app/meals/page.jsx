ce021799b7d0bcb00f52717d8241d9736b952d06
jai sharma <sharmajai901@gmail.com>
2024-02-21 00:00:52 +0530

    done till 112

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,25 +1,38 @@
-import Link from "next/link";
-import styles from "./page.module.css";
-import MealsGrid from "@/components/meals/mealsGrid/mealsGrid";
+import { Suspense } from 'react';
+import Link from 'next/link';
 
-const MealsPage = () => {
+import MealsGrid from '@/components/meals/mealsGrid/mealsGrid';
+
+import { getMeals } from '@/lib/meals';
+
+import styles from './page.module.css';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
         <h1>
-          Delicious meals created{" "}
+          Delicious meals, created{' '}
           <span className={styles.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
         <p className={styles.cta}>
-          <Link href="/meals/share">Share your recipe</Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]}/>
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
-};
-
-export default MealsPage;
+}
